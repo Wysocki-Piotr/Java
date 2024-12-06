d756b9235319d43ee9ca03e730dd0c91aff13259
Admin <01189063@pw.edu.pl>
2024-12-06 23:44:15 +0100

    dodanie dependency

---
+++ demo/src/main/java/API/Config.java
@@ -0,0 +1,13 @@
+package API;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class Config {
+    public static String getApiKey() throws IOException {
+        Properties properties = new Properties();
+        properties.load(new FileInputStream("config.properties"));
+        return properties.getProperty("api.key");
+    }
+}
--- demo/src/main/java/module-info.java
+++ demo/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module org.example.demo {
     requires javafx.controls;
     requires javafx.fxml;
+    requires okhttp3;
 
 
     opens org.example.demo to javafx.fxml;

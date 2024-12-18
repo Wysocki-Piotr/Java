bc20c7b0b8f3ba92933ba3ef416cd659be815615
bzabk <bzabkowski@gmail.com>
2024-12-18 02:24:17 +0100

    overriding main branch

--- demo/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module org.example.demo {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires okhttp3;
-
-
-    opens org.example.demo to javafx.fxml;
-    exports org.example.demo;
-}

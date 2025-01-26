ef834c19e7e3a68d3f6c64c17f1a72b154598a47
Admin <01189063@pw.edu.pl>
2025-01-26 11:29:27 +0100

    Dodanie save na glowny block z powrotem

--- src/main/java/Components/Components.java
+++ src/main/java/Components/Components.java
@@ -101,7 +101,7 @@ public class Components {
     protected List<Control> onStartBlock = new ArrayList<>(Arrays.asList(label1, label2, label3, textLoginEmail, textLoginPass, buttonRegister, buttonEnter));
 
     protected List<Node> secondBlock = new ArrayList<>(Arrays.asList(label4, textFavoritePlace, buttonEnter2, show,
-            deleteAccount, label5, logOut, comboBox, label6, label7, comboBoxCountries, gridPane2,
+            deleteAccount, label5, logOut, comboBox, label6, label7, comboBoxCountries, gridPane2, save,
             filter, textMin, textMax, result1, result2, result3, img1, img2, img3, alert1, alert2, alert3, gridPane, imageView, clippyLabel));
 
     protected AnimationTimer timer;
